b25ad075a001135a74fe8e357845c737498d79ae
Nishidh25 <shekhawatnishidh5@gmail.com>
2021-06-21 06:39:46 +0530

    Reset and Logout Functions added and linked

--- app/src/main/java/com/bd/bizhub/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/bd/bizhub/ui/profile/ProfileFragment.java
@@ -1,17 +1,28 @@
 package com.bd.bizhub.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.bd.bizhub.LoginActivity;
+import com.bd.bizhub.NavigationActivity;
+import com.bd.bizhub.OnboardingActivity;
 import com.bd.bizhub.R;
+import com.bd.bizhub.RealmDb;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
+
+import io.realm.mongodb.App;
+import io.realm.mongodb.User;
 
 public class ProfileFragment extends Fragment {
     View root;
@@ -19,7 +30,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-
+        App app = ((RealmDb) getActivity().getApplication()).getApp();
 
         root = inflater.inflate(R.layout.fragment_profile, container, false);
 
@@ -32,6 +43,9 @@ public class ProfileFragment extends Fragment {
         TextInputLayout weight_tl = root.findViewById(R.id.weight);
         TextInputLayout height_tl = root.findViewById(R.id.height);
         Button update = root.findViewById(R.id.update);
+
+        FloatingActionButton reset = root.findViewById(R.id.floatingActionButtonedit);
+        FloatingActionButton logout = root.findViewById(R.id.floatingActionButton);
 
         update.setOnClickListener(v -> {
             if(!set){
@@ -47,6 +61,42 @@ public class ProfileFragment extends Fragment {
             height_tl.setEnabled(set);
             weight_tl.setEnabled(set);
         });
+
+
+        reset.setOnClickListener(v->{
+            Thread thread = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    try  {
+                        app.getEmailPassword().callResetPasswordFunction("aa@gmail.com","abcd11111");
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            Toast.makeText(getContext(),"Password reset to: " +"abcd11111",Toast.LENGTH_LONG).show();
+            thread.start();
+        });
+
+        logout.setOnClickListener(v->{
+            User user = app.currentUser();
+            user.logOutAsync( result -> {
+                if (result.isSuccess()) {
+                    Log.v("AUTH", "Successfully logged out.: ID:"+ user.getId());
+                    startActivity(new Intent(getContext(), OnboardingActivity.class));
+                    getActivity().finish();
+
+                } else {
+                    Log.e("AUTH", "Log out failed! Error: " + result.getError().toString());
+                }
+            });
+        });
+
+
+
+
         return root;
     }
 
